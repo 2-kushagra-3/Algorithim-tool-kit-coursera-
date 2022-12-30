515018b5f52cb1aa59a0a30514c79815e0426b5c
Kushagra verma <kushagraid@gmail.com>
2022-12-30 19:25:16 +0530

    code added

---
+++ week 2/lastDigitFibo.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template< typename Type >
+class Solution {
+public:
+
+    using VT = vector< Type >;
+
+    Type fibonacci( Type N ){
+        if( N < 2 )
+            return N;
+        VT dp( N+1 );
+        dp[ 0 ] = 0;
+        dp[ 1 ] = 1;
+        for( auto i{ 2 }; i <= N; ++i )
+            dp[ i ] = ( dp[ i-2 ] + dp[ i-1 ] ) % 10;
+        return dp[ N ];
+    }
+};
+
+int main(){
+    using Type = int;
+    Solution< Type > solution;
+    Type N{ 0 };
+    cin >> N;
+    auto ans = solution.fibonacci( N );
+    cout << ans << endl;
+    return 0;
+}
